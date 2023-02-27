9890589c351eaa3f7ca310c01cc5f747ddedbf2d
Hemang Sinha <55807508+hemangdtu@users.noreply.github.com>
2023-02-27 18:04:33 +0530

    Studied Express, Starting with REST APIs and CRUD

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
@@ -6,36 +5,52 @@ const productPage = fs.readFileSync("product.html", "utf-8");
 const jsonData = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const products = jsonData.products;
 
-const server = http.createServer((req, res) => {
-    if (req.url.startsWith('/product')) {
-        const id = req.url.split('/')[2];
-        const productDetails = products.find(p => p.id === (+id));
-        res.setHeader('Content-Type', 'text/html');
-        let modifiedProductPage = productPage.replace('**title**', productDetails.title).replace('**url**', productDetails.thumbnail).replace('**price**', productDetails.price).replace('**rating**', productDetails.rating);
-        res.end(modifiedProductPage);
-        return;
-    }
+const express = require('express');
 
-    switch (req.url) {
-        case '/':
-            res.setHeader('Content-Type', 'text/html');
-            res.end(index);
-            break;
-        // case '/product':
-        //     res.setHeader('Content-Type', 'text/html');
-        //     let modifiedProductPage = productPage.replace('**title**', product.title).replace('**url**', product.thumbnail).replace('**price**', product.price).replace('**rating**', product.rating);
-        //     res.end(modifiedProductPage);
-        //     break;
-        case '/api':
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(jsonData));
-            break;
-        default:
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('<H1>PAGE NOT FOUND!</H1>')
-    }
-    console.log("Server started!");
+const server = express();
+
+server.listen(8080, () => {
+    console.log('Server started!');
 });
 
-server.listen(8080);
+// ----------------- MIDDLEWARES -----------------
+server.use(express.json()); // BUILT-IN MIDDLEWARES
 
+server.use((req, res, next) => {    // APPLICATION LEVEL MIDDLEWARE
+    console.log(req.method, req.ip, req.hostname);
+    next();
+});
+
+const basicAuth = (req, res, next) => { // ROUTERLEVEL MIDDLEWARE
+    if(req.body.password == '123') {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+// ERROR HANDLING MIDDLEWARE
+
+// THIRD-PARTY MIDDLEWARE
+
+// ----------------- APIs - ENDPOINTS - ROUTES -----------------
+server.get('/', (req, res)=>{
+    //  res.send('Welcome to the homepage.');
+     res.json({type:'GET'});
+ });
+
+ server.post('/', basicAuth, (req, res) => {
+    res.json({type: 'POST'});
+ });
+
+ server.delete('/', (req, res) => {
+    res.json({type: 'DELETE'});
+ });
+
+ server.put('/', (req, res) => {
+    res.json({type: 'PUT'});
+ });
+
+ server.patch('/', (req, res) => {
+    res.json({type: 'PATCH'});
+ });
