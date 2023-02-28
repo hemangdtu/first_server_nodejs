338347f4433a589c0e8cb3c54478d5ebe6e57328
Hemang Sinha <55807508+hemangdtu@users.noreply.github.com>
2023-02-28 18:52:34 +0530

    Done with REST APIs and CRUD, Starting MVC arch

--- index.js
+++ index.js
@@ -3,54 +3,62 @@ const fs = require("fs");
 const index = fs.readFileSync("index.html", "utf-8");
 const productPage = fs.readFileSync("product.html", "utf-8");
 const jsonData = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const products = jsonData.products;
 
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const server = express();
+
+const products = jsonData.products;
 
 server.listen(8080, () => {
     console.log('Server started!');
 });
 
-// ----------------- MIDDLEWARES -----------------
-server.use(express.json()); // BUILT-IN MIDDLEWARES
+server.use(express.json());
 
-server.use((req, res, next) => {    // APPLICATION LEVEL MIDDLEWARE
-    console.log(req.method, req.ip, req.hostname);
-    next();
+// REST APIs
+
+// Create POST /products
+server.post('/products', (req, res) => {
+    console.log(req.body);
+    products.push(req.body);
+    res.status(201).json(req.body)
 });
 
-const basicAuth = (req, res, next) => { // ROUTERLEVEL MIDDLEWARE
-    if(req.body.password == '123') {
-        next();
-    } else {
-        res.sendStatus(401);
-    }
-};
+// Read GET /products
+server.get('/products', (req, res) => {
+    res.status(200).json(products);
+});
 
-// ERROR HANDLING MIDDLEWARE
+// Read GET /products/:id
+server.get('/products/:id', (req, res) => {
+    const productId = +req.params.id;
+    const productDetails = products.find(p => p.id === productId);
+    res.status(200).json(productDetails);
+});
 
-// THIRD-PARTY MIDDLEWARE
+// Update PUT /products/:id
+server.put('products/:id', (req, res) => {
+    const productId = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === productId);
+    products.splice(productIndex, 1, {...req.body, id: productId});
+    res.status(201).json({message: "Data updated successfully!"});
+});
 
-// ----------------- APIs - ENDPOINTS - ROUTES -----------------
-server.get('/', (req, res)=>{
-    //  res.send('Welcome to the homepage.');
-     res.json({type:'GET'});
- });
+// Update PATCH /products/:id
+server.patch('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const productIndex = products.findIndex(p => p.id = productId);
+    products.splice(productIndex, 1, {...products[productIndex], ...req.body});
+    res.status(201).json({message: "Data updated successfully!"});
+});
 
- server.post('/', basicAuth, (req, res) => {
-    res.json({type: 'POST'});
- });
-
- server.delete('/', (req, res) => {
-    res.json({type: 'DELETE'});
- });
-
- server.put('/', (req, res) => {
-    res.json({type: 'PUT'});
- });
-
- server.patch('/', (req, res) => {
-    res.json({type: 'PATCH'});
- });
+// Delete DELETE /products/:id
+server.delete('/products/:id', (req, res) => {
+    const productId = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === productId);
+    products.splice(productIndex, 1);
+    res.status(200).json({message: "Data deleted successfully!"});
+});
